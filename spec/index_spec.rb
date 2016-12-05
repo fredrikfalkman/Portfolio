8de4d26f12d248872992d1c4fc75641adb8c0e06
fredrikfalkman <ff@ffd.se>
2016-12-05 22:37:13 +0100

    adds layout, footer, specs

---
+++ spec/index_spec.rb
@@ -0,0 +1,13 @@
+require './source/index.html.haml'
+
+describe projects do
+  
+end
+
+it 'displays project list' do
+    expect(page).to have_css '.projects'
+    within '.projects' do
+      expect(page).to have_content 'Test 1'
+      expect(page).to have_content 'Test 2'
+  end
+end
