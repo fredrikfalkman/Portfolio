e7300f601772b2d4240c248143b8bd18b1a2b63b
fredrikfalkman <ff@ffd.se>
2016-12-09 13:29:20 +0100

    changes filenames fron å to a

--- source/javascripts/all.js
+++ source/javascripts/all.js
@@ -2,8 +2,7 @@
 var didScroll;
 var lastScrollTop = 0;
 var delta = 5;
-// var navbarHeight = $('header'.outerHeight();
-var navbarHeight = 40;
+var navbarHeight = $('header'.outerHeight();
 
 $(window).scroll(function(event){
     didScroll = true;
